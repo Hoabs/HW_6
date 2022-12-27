55cb910455953f7f50f05563fa6f3d3f1d3e7e0e
Arsen <hoabs@mail.ru>
2022-12-27 21:14:10 +0300

    Initial

---
+++ HW_6.1/Program.cs
@@ -0,0 +1,12 @@
+﻿double[] a = Console.ReadLine().Split(" ").Select(x=>double.Parse(x)).ToArray();
+int count = 0;
+
+for (int i = 0; i < a.Length; i++)
+{
+    if (a[i]>0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
---
+++ HW_6.2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter k1 value");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter b1 value");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter k2 value");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Enter b2 value");
+double b2 = double.Parse(Console.ReadLine());
+
+double x = (b2-b1)/(k1-k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"({x}, {y})");
+
